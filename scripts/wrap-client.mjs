c69e3382191450a32ef4984bac1654bf0e7f00da
hackerFish <476829948@qq.com>
2026-08-17 01:00:22 +0800

    client: module/exports 声明修复

--- scripts/wrap-client.mjs
+++ scripts/wrap-client.mjs
@@ -6,6 +6,6 @@ import { readFileSync, writeFileSync } from 'node:fs'
 const PKG_ID = '@hackerfish/dsh-video-studio'
 const file = new URL('../lib/client/index.js', import.meta.url)
 const body = readFileSync(file, 'utf8')
-const wrapped = `window.__ModuleLoader__.load({\n  id: "${PKG_ID}",\n  factory: (require) => {\n${body}\n    return module.exports;\n  }\n});\n`
+const wrapped = `window.__ModuleLoader__.load({\n  id: "${PKG_ID}",\n  factory: (require) => {\n    var module = { exports: {} };\n    var exports = module.exports;\n${body}\n    return module.exports;\n  }\n});\n`
 writeFileSync(file, wrapped)
 console.log('client bundle wrapped: __ModuleLoader__.load(' + PKG_ID + ')')
